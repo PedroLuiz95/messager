967b28ce14e7cbb0eb06a67febee5ae6de95916e
PedroLuiz95 <pedroluizmm95@gmail.com>
2022-09-25 02:35:34 -0300

    adding cleaned trust numbers

--- src/assets/factory/cleanInadimplentNumbers.js
+++ src/assets/factory/cleanInadimplentNumbers.js
@@ -1,3 +1,28 @@
-export default function cleanInadimplentNumbers(arrayNumbers){
-  return arrayNumbers
+import handleTrustNumber from "./handleTrustNumber"
+export default async function cleanInadimplentNumbers(arrayNumbers) {
+  const now = new Date(Date.now())
+  const queryTrustNumbers = {
+    date_out: { $gte: now }
+  }
+  const trustNumbers = await handleTrustNumber('list_all',queryTrustNumbers)
+  const cleanNumbers = compareAndCleanNumbers(arrayNumbers,trustNumbers)
+  return cleanNumbers
+}
+function compareAndCleanNumbers(allNumbers,trustNumbers){
+  const nowNumberKey = 'numero'
+  const auxAllNumbers = allNumbers
+  for (let i in trustNumbers){
+    const nowTrustNumber = trustNumbers[i]
+    for (let j in allNumbers){
+      const nowNumber = allNumbers[j]
+      //Aqui é feita a comparação com o vetor que vem do mysql, a chave do numero pode alterar
+      if(nowTrustNumber.phone_number.localeCompare(nowNumber[nowNumberKey])){
+        const index = auxAllNumbers.indexOf(nowNumber)
+        if(index != -1 ){
+          auxAllNumbers.splice(index,1)
+        }
+      }
+    }
+  }
+  return auxAllNumbers
 }
--- src/assets/factory/handleTrustNumber.js
+++ src/assets/factory/handleTrustNumber.js
@@ -1,21 +1,28 @@
 import { mongoDb } from "../../assets/database.js"
 import trust_list from "../../models/trustList"
-export default async function handleTrustNumber(option,data={}) {
+export default async function handleTrustNumber(option, data = {}) {
   await mongoDb()
   let out
-  if(option == 'list_all'){
-    out = get_all_numbers()
-    console.log(out)
-  }else if (option == 'insert'){
+  if (option == 'list_all') {
+    out = await get_all_numbers(data)
+  } else if (option == 'insert') {
     await inserNumber(data)
   }
 
   return out
 }
-async function get_all_numbers(data={}){
-  const out = await trust_list(data)
+async function get_all_numbers(data = {}) {
+  const out = await trust_list.find(data)
   return out
 }
-async function inserNumber(data={}){
-  await trust_list.create(data)
+async function inserNumber(data = {}) {
+  const query = {
+    phone_number: data.phone_number
+  }
+  const check = await get_all_numbers(query)
+  if (check.length > 0) {
+    await trust_list.updateOne(query, { $set: data })
+  }else{
+    await trust_list.create(data)
+  }
 }
