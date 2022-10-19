65fbfc189150346c1c13c95b66c989bc9d4f7a79
PedroLuiz95 <pedroluizmm95@gmail.com>
2022-10-19 11:57:56 -0300

    changes

--- src/pages/teste/testeCleanTrustNumbers.js
+++
@@ -1,5 +0,0 @@
-import CleanTrustNumbers from "../../tasks/cleanTrustNumbers"
-export default async function handler(req, res) {
-  const totalDelete = await CleanTrustNumbers()
-  res.status(200).json(totalDelete)
-}
