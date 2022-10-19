f6c76880999d2f2a09387ef72e5cf8ff7e4f4c79
PedroLuiz95 <pedroluizmm95@gmail.com>
2022-10-18 22:04:03 -0300

    adding changes

--- src/pages/api/teste/testeMysqlInsert.js
+++
@@ -1,6 +0,0 @@
-import { mysqlDb } from "../../../assets/database"
-export default async function handler(req, res) {
-  const conn = await mysqlDb()
-  const out = await conn.query('select * from teste')
-  res.status(200).json()
-}
